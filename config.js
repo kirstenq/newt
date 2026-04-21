169a9a547c49b0475672b1eadbad21b3388ac447
kirstenq <kirstenguerra@gmail.com>
2026-04-21 18:17:37 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Newt configuration
-// Add your Anthropic API key below.
-// Get one at: https://console.anthropic.com
-
-window.NEWT_API_KEY = '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';
